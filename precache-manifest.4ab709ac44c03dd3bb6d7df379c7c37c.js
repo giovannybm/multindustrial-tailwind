a9d1f3d14dea8df986b7c469427807d02b635e98
Lexetam <giovannybm@gmail.com>
2020-08-18 22:51:16 -0500

    all

---
+++ precache-manifest.4ab709ac44c03dd3bb6d7df379c7c37c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f743c751fce7d1686e3b92ac6d1b4f",
+    "url": "/wordpress/wp-content/themes/multindustrial-tailwind/index.php"
+  },
+  {
+    "revision": "29f7b3782878e8511b91",
+    "url": "/wordpress/wp-content/themes/multindustrial-tailwind/static/css/main.chunk.css"
+  },
+  {
+    "revision": "61d4ee08c424421ba52c",
+    "url": "/wordpress/wp-content/themes/multindustrial-tailwind/static/js/2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wordpress/wp-content/themes/multindustrial-tailwind/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd09303542fa78f9d3d8",
+    "url": "/wordpress/wp-content/themes/multindustrial-tailwind/static/js/bundle.js"
+  },
+  {
+    "revision": "29f7b3782878e8511b91",
+    "url": "/wordpress/wp-content/themes/multindustrial-tailwind/static/js/main.chunk.js"
+  }
+]);
